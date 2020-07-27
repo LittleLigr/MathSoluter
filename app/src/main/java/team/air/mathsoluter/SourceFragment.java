ac16faad91679fc54095fec6c0fe7cc148a287db
Игорь Шадрин <igor@shadrins.ru>
2020-07-27 22:14:14 +0300

    Repair project

--- app/src/main/java/team/air/mathsoluter/SourceFragment.java
+++ app/src/main/java/team/air/mathsoluter/SourceFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -51,6 +52,7 @@ public class SourceFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
